535a4162d767b732eaf43031813fc286dc2f2fad
Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>
2024-06-05 14:48:00 +0000

    chore(maintainers): add maintainers list
    Signed-off-by: GitHub <noreply@github.com>

---
+++ maintainers/list.nix
@@ -0,0 +1,9 @@
+{
+  ajhalili2006 = {
+    name = "ajhalili2006";
+    email = "ajhalili2006@crew.recaptime.dev";
+    matrix = "@ajhalili2006:envs.net";
+    github = "ajhalili2006";
+    githubId = 34998342;
+  }
+}
